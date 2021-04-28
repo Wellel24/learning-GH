46f9448c8a53952bf3773c8a5a4917a39cc3a7a0
Elin <Wellel@student.breckschool.org>
2021-04-28 14:50:00 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Elin Wellmann on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
